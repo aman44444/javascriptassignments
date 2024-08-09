e27c5f1705e76a2e867c421ade08bd5cc14dac94
aman1357 <amancorp04@gmail.com>
2024-08-09 23:45:08 +0530

    .

---
+++ functions/functions.js
@@ -0,0 +1,14 @@
+let num1 = Number(prompt("Enter the first number"))
+let num2 = Number( prompt("Enter the second number"))
+
+function sum() {
+    return num1 + num2;
+}
+
+console.log("Sum", sum())
+
+function subtraction() {
+    return num1 - num2;
+}
+
+console.log("Subtraction", subtraction())
